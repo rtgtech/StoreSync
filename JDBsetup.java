c2f835000f76b26ab3c5d9c54ba68777f0126ba1
Rumaan Zameer <rtgtech.ai@gmail.com>
2025-09-03 20:03:14 +0530

    Update JDBsetup.java

--- JDBsetup.java
+++ JDBsetup.java
@@ -1,13 +1,10 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
-import java.time.LocalDate;
+import java.io.File;
+import java.sql.*;
 
-public class JDBsetup {
-    static String url = "jdbc:sqlite:storesync-db.db";
+public class JDBSetup {
+    static String url = "jdbc:sqlite:storesync.db";
 
-    public static void main(String[] args) {
+    public static void buildNewTable() {
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
                 int day = LocalDate.now().getDayOfMonth();
@@ -34,12 +31,14 @@ public class JDBsetup {
                         + "id INTEGER, curr_id INTEGER, curr_date INTEGER);";
 
                 Statement stmt = conn.createStatement();
+                System.out.println("Creating storesync.db ...");
                 stmt.execute(today_stat_table);
                 stmt.execute(monthly_stat_table);
                 stmt.execute(upto_date_stat_table);
                 stmt.execute(inventory_table);
                 stmt.execute(current);
-                
+                System.out.println("Created Tables ...");
+
                 String init_current = "INSERT INTO current (id, curr_id, curr_date) VALUES (1, 1, " + day + ");";
                 String init_1 = "INSERT INTO today_stats (id, sales, exps, profit, tot_goods_sld, cus_count) VALUES (1, 0.0, 0.0, 0.0, 0, 0);";
                 String init_2 = "INSERT INTO monthly_stats (id, sales, exps, profit, tot_goods_sld, cus_count) VALUES (1, 0.0, 0.0, 0.0, 0, 0);";
@@ -48,11 +47,24 @@ public class JDBsetup {
                 stmt.execute(init_1);
                 stmt.execute(init_2);
                 stmt.execute(init_3);
-
+                System.out.println("Initialized Tables ...");
+                
             }
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
+            System.out.println("Program abnormally terminated ...");
+            System.exit(1);
         }
+
+    }
+
+    public static void main(String[] args) {
+        File file = new File("storesync.db");
+        if (!file.exists()){
+            System.out.println("storesync.db not found. Creating storesync.db ...");
+            buildNewTable();
+        }
+        
     }
 }
